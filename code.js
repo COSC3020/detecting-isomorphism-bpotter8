5db3d84d79c1a123d17b5d6e8a6fbbace58d3ec1
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-04-18 22:04:57 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+function are_isomorphic(graph1, graph2) {
+    const tracker = [];
+    
+    if(graph1.length != graph2.length){
+        return false;
+    }
+    
+    for (var i = 0; i < graph1.length; i++){
+        var compareFirst = graph1[i];
+        var compareSecond = graph2[i];
+        
+        if (compareFirst.length != compareSecond.length){
+            return false;
+        }
+        
+        for (var j = 0; j < compareFirst.length; j++){
+            var ind = compareFirst[j];
+            
+            if (tracker[ind] == undefined){
+                tracker[ind] = compareSecond[j];
+            } else if (tracker[ind] != compareSecond[j]){
+                return false;
+            }
+        }
+    }
+    
+    return true;
 }
